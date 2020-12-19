e0d5d66754e8d4f89b92cb8d419b74823eee58b8
Jovidon <jova_94@inbox.ru>
2020-12-19 07:35:49 -0800

    ORM- ORM-CinemaLab

---
+++ Spring-12-ORM-CinemaLab/src/main/java/com/cyber/enums/MovieType.java
@@ -0,0 +1,5 @@
+package com.cyber.enums;
+
+public enum MovieType {
+    PREMIER,REGULAR;
+}
---
+++ Spring-12-ORM-Mapping-ManyToMany/src/main/java/com/cyber/entity/Tag.java
@@ -0,0 +1,29 @@
+package com.cyber.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags", cascade = CascadeType.ALL)
+    private Set<Post> posts=new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
